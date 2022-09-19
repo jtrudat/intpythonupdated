1432cc7a5e5decc7976234d0d6143aabe25a90ea
Daniel Ortiz <DOrtiz1011@gmail.com>
2022-09-19 09:08:25 -0500

    day8

---
+++ Day8/Activity_1_Bubble_Sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(list):
+    return list
+
+# Code to test function
+sample_list = [1, 5, 2, 6, 7]
+print(f"Unsorted list: {sample_list}")
+bubble_sort(sample_list)
+print(f"Sorted list: {sample_list}")
