2668224c1c4042d324d388c4a1240b60f8075941
Daniel Ortiz <DOrtiz1011@gmail.com>
2022-10-16 15:20:17 -0500

    more solutions and HW2

---
+++ Homework/Day2_HW/HW_2_Python_Function_Practice_Part_4.py
@@ -0,0 +1,45 @@
+# Write a Python function called max_num()to find the Max of three numbers.
+
+def max_num(a,b,c):
+    pass
+
+print(max_num(1,2,3))
+print(max_num(100,50,1))
+print(max_num(15,30,2))
+
+# Write a Python function called mult_list()  to multiply all the numbers in a list.
+
+def mult_list(lst):
+    pass
+
+print(mult_list([1,2,3]))
+print(mult_list([]))
+print(mult_list([15]))
+
+# Write a Python function called rev_string() to reverse a string.
+
+def rev_string(my_str):
+    pass
+
+print(rev_string(""))
+print(rev_string("apple"))
+print(rev_string("mt string"))
+
+# Write a Python function called num_within() to check whether a number falls in a given range.
+
+def num_within(x,a,b):
+    pass
+
+print(num_within(3,2,4))
+print(num_within(3,1,3))
+print(num_within(10,2,5))
+
+# Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+
+triangle = [[1],[1,1]]
+
+def pascal(n):
+    pass
+
+pascal(2)
+pascal(5)
